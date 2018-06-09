1ec1b65a13e3818bc74e49549a6489014dd171c5
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-09 00:58:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
   
-  if int % 3 
+  if int % 3 == 0
     return "Fizz"
   end
   
-  if int % 5
+  if int % 5 == 0 
     return "Buzz"
   end 
   
-  if int % 3 && 5
+  if int % 3 && 5 == 0
     return "FizzBuzz"
   end 
   
